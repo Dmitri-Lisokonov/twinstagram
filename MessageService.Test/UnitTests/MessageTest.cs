4a3fa5e2b5b0d4660e37d37cbdee4859ef0efcd5
Dmitri <Dmitri.lisokonov@gmail.com>
2022-04-12 14:47:47 +0200

    Testing CI/CD

---
+++ MessageService.Test/UnitTests/MessageTest.cs
@@ -0,0 +1,36 @@
+using MessageService.Context;
+using MessageService.Controllers;
+using MessageService.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UserService.Test
+{
+    public class MessageTest
+    {
+        private readonly MessageController _controller;
+        private readonly MessageServiceDatabaseContext _context;
+
+        public MessageTest()
+        {
+            var options = new DbContextOptionsBuilder<MessageServiceDatabaseContext>().UseInMemoryDatabase(databaseName: "InMemoryUserServiceDatabase").Options;
+            var context = new MessageServiceDatabaseContext(options);
+            var controller = new MessageController(context);
+            createUsersInMemoryDatabase();        
+        }
+
+        [Fact]
+        public void TestingPipeLine()
+        {
+            Assert.Equal(1, 1);
+        }
+
+        private static void createUsersInMemoryDatabase()
+        {
+
+        }
+    }
+}
--- MessageService/Program.cs
+++ MessageService/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<MessageServiceDatabaseContext>(options =>
 {
     options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+//test
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
