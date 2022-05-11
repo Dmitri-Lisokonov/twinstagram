4cdb86f98e3a4a71067e796a0fda12685d86687a
Dmitri <Dmitri.lisokonov@gmail.com>
2022-05-11 21:14:33 +0200

    prepare deploy

--- AuthenticationService/Context/AuthServiceDatabaseContext.cs
+++ AuthenticationService/Context/AuthServiceDatabaseContext.cs
@@ -21,28 +21,27 @@ namespace AuthenticationService.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Connect to Azure SQL Database if deployed
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//                var connectionString = Environment.GetEnvironmentVariable("db-connection-string");
-//                if (string.IsNullOrWhiteSpace(connectionString))
-//                {
-//                    throw new MissingFieldException("Database environment variable not found.");
-//                }
-//                else
-//                {
-//#pragma warning disable CS8602 // Dereference of a possibly null reference.
-//                    optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("db-connection-string").Replace("DATABASE_NAME", "message-service_db"));
-//#pragma warning restore CS8602 // Dereference of a possibly null reference.
-//                }
-//            }
-            
-            //Connect to MySQL database for development
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseMySql("server=localhost;Port=3306;uid=admin;pwd=123Welkom!;database=authentication;", new MySqlServerVersion(new Version(8, 0, 28)));
+                var connectionString = Environment.GetEnvironmentVariable("db-connection-string");
+                if (string.IsNullOrWhiteSpace(connectionString))
+                {
+                    throw new MissingFieldException("Database environment variable not found.");
+                }
+                else
+                {
+                    optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("db-connection-string").Replace("DATABASE_NAME", "authentication-service_db"));
+
+                }
             }
 
-            base.OnConfiguring(optionsBuilder);
+            //Connect to MySQL database for development
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseMySql("server=localhost;Port=3306;uid=admin;pwd=123Welkom!;database=authentication;", new MySqlServerVersion(new Version(8, 0, 28)));
+            //}
+
+            //base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- UserService/Context/UserServiceDatabaseContext.cs
+++ UserService/Context/UserServiceDatabaseContext.cs
@@ -19,26 +19,26 @@ namespace UserService.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //Connect to Azure SQL Database if deployed
-//            if (!optionsBuilder.IsConfigured)
-//            {
-//                var connectionString = Environment.GetEnvironmentVariable("db-connection-string");
-//                if (string.IsNullOrWhiteSpace(connectionString))
-//                {
-//                    throw new MissingFieldException("Database environment variable not found.");
-//                }
-//                else
-//                {
-//                    optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("db-connection-string").Replace("DATABASE_NAME", "message-service_db"));
-//                }
-//            }
-            
-            //Connect to MySQL database for development
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseMySQL("server=localhost;Port=3306;uid=admin;pwd=123Welkom!;database=userservice_db;");
+                    var connectionString = Environment.GetEnvironmentVariable("db-connection-string");
+                    if (string.IsNullOrWhiteSpace(connectionString))
+                    {
+                        throw new MissingFieldException("Database environment variable not found.");
+                    }
+                    else
+                    {
+                        optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("db-connection-string").Replace("DATABASE_NAME", "user-service_db"));
+                    }
             }
 
-            base.OnConfiguring(optionsBuilder);
+            //Connect to MySQL database for development
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseMySQL("server=localhost;Port=3306;uid=admin;pwd=123Welkom!;database=userservice_db;");
+            //}
+
+            //base.OnConfiguring(optionsBuilder);
         }
 
         public DbSet<ApplicationUser> Users { get; set; }
