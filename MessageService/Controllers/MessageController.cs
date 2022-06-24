8decd5939986bf7035085d3865f0ace7d7bee8e8
Dmitri <Dmitri.lisokonov@gmail.com>
2022-06-24 15:01:38 +0200

    bug fix

--- MessageService/Controllers/MessageController.cs
+++ MessageService/Controllers/MessageController.cs
@@ -34,7 +34,7 @@ namespace MessageService.Controllers
         [Authorize]
         public async Task<IActionResult> GetMessages(Guid userId)
         {
-            var messages = await _dbContext.Messages.ToListAsync();
+            var messages = await _dbContext.Messages.Where(x => x.UserId == userId).ToListAsync();
             if (messages.Any())
             {
                 var messagesDto = _mapper.Map<IEnumerable<Message>, IEnumerable<MessageDto>>(messages);
